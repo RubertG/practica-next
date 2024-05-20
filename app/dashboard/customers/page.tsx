48b95d16d9cbdb8132055f8b96713f7ab5ce8869
Rubert_G <rubertgonzalez5@gmail.com>
2024-05-20 11:26:21 -0500

    Mi primer commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+function Page() {
+  return (
+    <div><p>Customers Page</p></div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+function Page() {
+  return (
+    <div><p>Invoices Page</p></div>
+  )
+}
+
+export default Page
